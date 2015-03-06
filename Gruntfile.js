2a6bbeef2f7c67f04b2f4d4042c0b0d07cc59a7f
Rob <rob@pizelstew.co.uk>
2015-03-06 21:57:30 +0000

    Added normalise

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
       },
       options: {
         sourceMap: true, 
-        outputStyle: 'nested', 
+        outputStyle: 'compressed', 
         imagePath: "../",
       }
     },
